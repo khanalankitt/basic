46d773539efeb97270e7290e0212cad275617c37
Ankit Khanal <72298204+khanalankitt@users.noreply.github.com>
2024-06-08 22:51:45 +0545

    add

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const Fact = require('./models/blog'); 
 const app = express();
+const path = require('path');
 
 app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
